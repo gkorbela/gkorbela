773a9ca95e4abe3c4b0073fcdfbef6c10eb82494
gkorbela <58258465+gkorbela@users.noreply.github.com>
2024-08-16 12:10:50 +0200

    Utworzono za pomocą Colab

---
+++ LC_rnn.R
@@ -0,0 +1,41 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "gpuType": "V28",
+      "authorship_tag": "ABX9TyO5Ns3L+mA+AM6r7bKxwWo6",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "ir",
+      "display_name": "R"
+    },
+    "language_info": {
+      "name": "R"
+    },
+    "accelerator": "TPU"
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/gkorbela/gkorbela/blob/master/LC_rnn.R\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "6irIHknuqgYX"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
